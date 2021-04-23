215075f036796397151f197b6b8f51514c2e23a1
EmiyaC <707598245@qq.com>
2021-04-23 09:45:15 +0800

    22. 添加 Lombok 工具，并添加 获取头像链接 功能

--- src/main/java/com/emiyacc/luntan/mapper/QuestionsMapper.java
+++ src/main/java/com/emiyacc/luntan/mapper/QuestionsMapper.java
@@ -2,11 +2,9 @@ package com.emiyacc.luntan.mapper;
 
 import com.emiyacc.luntan.model.Question;
 import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public interface QuestionsMapper {
-    @Insert("insert into questions (title, description, creator, tag, gmt_create, gmt_modified)" +
-            "value (#{title}, #{description}, #{creator}, #{tag}, #{gmtCreate}, #{gmtModified})")
+    @Insert("insert into questions (title, description, creator, tag, gmt_create, gmt_modified, avatar_url)" +
+            "value (#{title}, #{description}, #{creator}, #{tag}, #{gmtCreate}, #{gmtModified}, #{avatarUrl})")
     void create(Question question);
 }
